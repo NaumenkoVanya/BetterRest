758f0c0745aaeefb00e3ed2cee310e555e458cb3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-13 12:04:48 +0300

    add new project

---
+++ BetterRestMy/BetterRestMy/BetterRestMyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestMyApp.swift
+//  BetterRestMy
+//
+//  Created by Ваня Науменко on 13.02.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestMyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
