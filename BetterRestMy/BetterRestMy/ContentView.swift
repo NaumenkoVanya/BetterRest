2699338093f44ebbf27af1cdbce34b4ff55289a5
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-14 20:30:43 +0300

    add @State sleepAmountTwo, DatePicker

--- BetterRestMy/BetterRestMy/ContentView.swift
+++ BetterRestMy/BetterRestMy/ContentView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var sleepAmountTwo = 9.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         VStack {
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+            Stepper("\(sleepAmountTwo.formatted()) hours", value: $sleepAmountTwo, in: 4...12, step: 0.25)
         }
+        
         .padding()
     }
 }
