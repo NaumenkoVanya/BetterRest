d222045f7f4a98366ba66d199ff79ad21016012b
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-14 23:26:04 +0300

    add Date

--- BetterRestMy/BetterRestMy/ContentView.swift
+++ BetterRestMy/BetterRestMy/ContentView.swift
@@ -11,9 +11,15 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var sleepAmountTwo = 9.0
     @State private var wakeUp = Date.now
+    var someDate = Calendar(identifier: .gregorian)
+    @State private var components = DateComponents()
     
     var body: some View {
         VStack {
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.hour().minute().day().month())
             DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
